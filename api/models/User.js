9737c1407a6f9a24ccd30c988ba23fc9fce60999
Dennis Baskin <dennis@dennisbaskin.com>
2017-02-05 01:43:35 -0700

    06-custom-routing recreate User model

--- api/models/User.js
+++ api/models/User.js
@@ -4,14 +4,38 @@
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
+const uuid = require('node-uuid');
 
 module.exports = {
+  tableName: 'users',
+
   attributes: {
-    email: {
-      type: 'string',
-      required: true,
+    username: {
+      maxLength: 16,
+      minLength: 3,
       notNull: true,
-      defaultsTo: "",
-    }
+      required: true,
+      type: 'string',
+      unique: true,
+    },
+
+    email: {
+      notNull: true,
+      required: true,
+      type: 'email',
+      unique: true,
+    },
+
+    uuid: {
+      notNull: true,
+      type: 'uuid',
+      defaultsTo: () => uuid.v4(),
+    },
+
+    password: {
+      notNull: true,
+      required: true,
+      type: 'string',
+    },
   }
 };
